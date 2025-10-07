0096a6c658a0e556297c6b44a3748fdf46d6d3c0
Yaseen Khan <gul494419@gmail.com>
2025-10-07 19:06:17 +0500

    q

--- Backend/app.js
+++ Backend/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 config({ path: "./config/config.env"});
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: process.env.FRONTEND_URL?.trim(),
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
