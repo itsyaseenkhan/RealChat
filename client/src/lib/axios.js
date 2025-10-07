15cb75358696db830823d594f8144ad99f0b2a75
Yaseen Khan <gul494419@gmail.com>
2025-10-07 17:02:51 +0500

    e

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-   baseURL: import.meta.env.MODE === "development"  ? "http://localhost:5000/api/v1"  : "/",
+   baseURL: import.meta.env.MODE === "development"  ? "https://realchat-backends-9v8u.onrender.com/api/v1"  : "/",
    withCredentials: true,
 });
 
--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -8,7 +8,7 @@ export const connectSocket = (userId) => {
   if (!userId) return;
 
   socket = io(
-    import.meta.env.MODE === "development" ? "http://localhost:5000" : "https://realchat-backends-9v8u.onrender.com",
+    import.meta.env.MODE === "development" ? "https://realchat-backends-9v8u.onrender.com" : "/",
     {
       query: {userId},    
     }
