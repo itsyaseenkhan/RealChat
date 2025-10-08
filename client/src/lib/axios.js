8aedd7bb16292bb1475518663761106de0d249e8
Yaseen Khan <gul494419@gmail.com>
2025-10-08 10:28:45 +0500

    f

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development"? "http://localhost:5000/api/v1" : "https://realchat-backends-9v8u.onrender.com/api/v1",
+  baseURL: "https://realchat-backends-9v8u.onrender.com/api/v1",
   withCredentials: true,
 });
+
+
